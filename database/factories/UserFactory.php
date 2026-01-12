1014466f2f5f9762d1bc7d1c478eda0205aa1e1e
Arun Kamaraj <arunkamaraj.zerosoft@gmail.com>
2026-01-12 10:16:16 +0530

    Update: UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,7 +26,7 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'name' => 'Test User',
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
