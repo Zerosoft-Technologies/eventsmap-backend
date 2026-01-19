ef8c0df3bf24f275ab9dd5712e0a831431254ed7
Arun Kamaraj <arunkamaraj.zerosoft@gmail.com>
2026-01-19 17:37:46 +0530

    Update: cors

--- config/cors.php
+++ config/cors.php
@@ -24,6 +24,7 @@ return [
         'http://localhost:3000',
         'http://localhost:3001',
         'http://185.133.88.194:3001',
+        'https://admin.eventsmap.zerosoft.com',
     ],
 
     'allowed_origins_patterns' => [],
