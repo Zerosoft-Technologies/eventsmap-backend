befc62ea54778ecf1e7d48f01e37db90c1f91d3c
Maharajazerosoft <maharajas.zerosoft@gmail.com>
2026-03-14 15:26:37 +0530

    Remove unused Sanctum migration ignore in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,7 +6,6 @@ use App\Models\EventV2;
 use App\Policies\EventV2Policy;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
-use Laravel\Sanctum\Sanctum;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,8 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        // We use our own migration (2025_01_12_000005). Ignore Sanctum's so no duplicate table.
-        Sanctum::ignoreMigrations();
+        //
     }
 
     /**
