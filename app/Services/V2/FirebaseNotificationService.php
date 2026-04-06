745649a5f2d39beb6b99e1541d54eb8a7cc64266
Maharajazerosoft <maharajas.zerosoft@gmail.com>
2026-04-06 10:48:26 +0530

    Update the notification

--- app/Services/V2/FirebaseNotificationService.php
+++ app/Services/V2/FirebaseNotificationService.php
@@ -56,7 +56,7 @@ class FirebaseNotificationService
         $docId = $this->documentId($invitation->id);
         $receiverId = (string) $invitation->receiver_id;
         $senderName = $invitation->sender?->name ?? 'Someone';
-        $message = "You have a pending invitation from {$senderName}. Please check your email to accept the invitation.";
+        $message = "You have a pending invitation from {$senderName}. Please log in to The Events Map to accept or decline the invitation.";
 
         $fields = [
             'receiver_id' => ['stringValue' => $receiverId],
